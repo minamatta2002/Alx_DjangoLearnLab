524d1dfedf89b2d3dfeaaac92ef7b3fe17ad9fac
minamatta2002 <your_email@example.com>
2025-07-20 20:19:45 +0300

    the second task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 from django.urls import reverse_lazy
 from .models import Book
 from .models import Library
