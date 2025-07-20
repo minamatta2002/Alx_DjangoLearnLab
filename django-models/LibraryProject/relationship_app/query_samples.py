a4f3225e1e173f1bab42613f87a09264e241f037
minamatta2002 <your_email@example.com>
2025-07-20 19:37:20 +0300

    the first task

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,30 @@
+from . import models
+
+book = models.Book
+author = models.Author
+library = models.Library
+librarian = models.Librarian  
+
+def get_books_by_author(author_name):
+    try:
+        author_instance = models.Author.objects.get(name=author_name)
+        return book.objects.filter(author=author_instance)
+    except models.Author.DoesNotExist:
+        return book.objects.none()
+    
+def get_books_in_library(library_name):
+    try:
+        library_instance = models.Library.objects.get(name=library_name)
+        return library_instance.books.all()
+    except models.Library.DoesNotExist:
+        return book.objects.none()
+    
+def get_librarian_of_library(library_name):
+    try:
+        library_instance = models.Library.objects.get(name=library_name)
+        return library_instance.librarian
+    except models.Library.DoesNotExist:
+        return None
+    except models.Librarian.DoesNotExist:
+        return None
+
