8e51b488d3910ad97d6dd1561317df00ac5518fe
minamatta2002 <your_email@example.com>
2025-07-20 20:19:02 +0300

    the second task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
 from django.urls import reverse_lazy
-from .models import  Book, Library
+from .models import Book
+from .models import Library
 
 # Create your views here.
 def book_list(request):
