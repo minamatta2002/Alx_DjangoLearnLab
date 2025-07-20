de398cc2f5390abf40a8bbacd0fd17ff833f3f18
minamatta2002 <your_email@example.com>
2025-07-20 20:13:59 +0300

    the second task

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,11 +6,11 @@ from .models import Author, Book, Library, Librarian
 # Create your views here.
 def book_list(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/templates/book_list.html', {'books': books})
+    return render(request, 'relationship_app/book_list.html', {'books': books})
 
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/templates/library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
     def get_context_data(self, **kwargs):
