b36368b67de187adb9c3834168ab6dafc9ab3b6a
minamatta2002 <mina.matta2002@gmail.com>
2025-07-27 15:04:36 +0300

    Fix: clean models.py and organize app files

--- LibraryProject/accounts/admin.py
+++ LibraryProject/accounts/admin.py
@@ -4,12 +4,16 @@ from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
+
     fieldsets = UserAdmin.fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
+
     add_fieldsets = UserAdmin.add_fieldsets + (
         (None, {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    list_display = ['username', 'email', 'date_of_birth', 'is_staff']
+
+    list_display = ['email', 'date_of_birth', 'is_staff']
+    ordering = ['email']
 
 admin.site.register(CustomUser, CustomUserAdmin)
--- LibraryProject/accounts/models.py
+++ LibraryProject/accounts/models.py
@@ -2,22 +2,30 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
             raise ValueError('Email must be set')
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        return self.create_user(username, email, password, **extra_fields)
+        return self.create_user(email, password, **extra_fields)
 
 class CustomUser(AbstractUser):
+    username = None  # نلغي اليوزرنيم
+    email = models.EmailField(unique=True)
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profiles/', null=True, blank=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
     objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
