daa20401170bffb73dfbbacd0b06100d90eb57b9
minamatta2002 <mina.matta2002@gmail.com>
2025-07-27 08:08:11 +0300

    Fix: clean models.py and organize app files

--- LibraryProject/bookshelf/models.py
+++ LibraryProject/bookshelf/models.py
@@ -1,10 +1,9 @@
-from django.db import models
 
-# Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
-    publication_year = models.IntegerField()
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    age = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__(self):
-        return f"{self.title} by {self.author} ({self.publication_year})"
+        return self.username
