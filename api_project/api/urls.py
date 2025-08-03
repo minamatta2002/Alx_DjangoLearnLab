476aea5920f61f0c528413314e11f92c45f86881
minamatta2002 <mina.matta2002@gmail.com>
2025-08-03 15:45:07 +0300

    4

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .views import BookList, BookViewSet
 
 router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # عرض قائمة الكتب فقط (ListAPIView)
     path('books/', BookList.as_view(), name='book-list'),
+
+    # جميع عمليات CRUD عبر ViewSet
     path('', include(router.urls)),
+
+    # Endpoint للحصول على التوكن
+    path('get-token/', obtain_auth_token, name='api_token_auth'),
 ]
