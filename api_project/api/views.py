6e1f30871ef6e070e5f3b758a430e89fd6bfcd6d
minamatta2002 <mina.matta2002@gmail.com>
2025-08-03 15:37:35 +0300

    3

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
+# هذا كلاس العرض فقط - ListAPIView
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# وهذا كلاس CRUD الكامل - ViewSet
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
