f0eeca9f16341f477763e6109f69f99e000df4e5
minamatta2002 <mina.matta2002@gmail.com>
2025-08-31 14:53:45 +0300

    task

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/mina/Alx_DjangoLearnLab/advanced_features_and_security/venv
+set -gx VIRTUAL_ENV /home/mina/Alx_DjangoLearnLab/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
